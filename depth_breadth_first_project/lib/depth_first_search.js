a5fec9e0cbbc27c51d321a6480ae7b44a8c78d34
Heiman Chan <heimanchan0@gmail.com>
2019-03-20 14:06:24 -0700

    Implement bfs and dfs

--- depth_breadth_first_project/lib/depth_first_search.js
+++ depth_breadth_first_project/lib/depth_first_search.js
@@ -1,5 +1,21 @@
 function depthFirstSearch(root, targetVal) {
+	// if (!root) return null;
 
+	// if (root.val === targetVal) return root;
+	// depthFirstSearch(root.left, targetVal);
+	// depthFirstSearch(root.right, targetVal);
+	let stack = [root];
+
+	while(stack.length) {
+		let node = stack.pop();
+
+		if (node.val === targetVal) return node;
+
+		if (node.right) stack.push(node.right);
+		if (node.left) stack.push(node.left)
+	}
+
+	return null;
 }
 
 
