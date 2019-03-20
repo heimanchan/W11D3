41057b4b5e11e0645974a05fd10d9fe0bde89260
Heiman Chan <heimanchan0@gmail.com>
2019-03-20 14:28:56 -0700

    Implement tree height

--- depth_breadth_first_project/lib/tree_sum.js
+++ depth_breadth_first_project/lib/tree_sum.js
@@ -1,19 +1,20 @@
 function treeSum(root) {
-	let stack = [root];
-	let result = 0;
+	// let stack = [root];
+	// let result = 0;
 
-	if (root === null) return 0;
-	while (stack.length) {
-		let node = stack.pop();
-		result += node.val;
+	// if (root === null) return 0;
+	// while (stack.length) {
+	// 	let node = stack.pop();
+	// 	result += node.val;
 
-		if (node.right) stack.push(node.right);
-		if (node.left) stack.push(node.left);
-	}
+	// 	if (node.right) stack.push(node.right);
+	// 	if (node.left) stack.push(node.left);
+	// }
 
-	return result;
+	// return result;
+	if (!root) return 0;
+	return treeSum(root.left) + root.val + treeSum(root.right);
 }
-
 
 module.exports = {
 	treeSum
