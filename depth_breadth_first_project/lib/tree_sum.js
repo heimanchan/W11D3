936bdff6c2135e71c63917b58b63bcac1d27de12
Heiman Chan <heimanchan0@gmail.com>
2019-03-20 14:12:29 -0700

    Implement tree sum

--- depth_breadth_first_project/lib/tree_sum.js
+++ depth_breadth_first_project/lib/tree_sum.js
@@ -1,8 +1,20 @@
 function treeSum(root) {
+	let stack = [root];
+	let result = 0;
 
+	if (root === null) return 0;
+	while (stack.length) {
+		let node = stack.pop();
+		result += node.val;
+
+		if (node.right) stack.push(node.right);
+		if (node.left) stack.push(node.left);
+	}
+
+	return result;
 }
 
 
 module.exports = {
-    treeSum
+	treeSum
 };
