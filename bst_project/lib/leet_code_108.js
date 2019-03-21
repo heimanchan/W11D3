bf17041a102f46fa0404ffe8b6da916f76a67778
Heiman Chan <heimanchan0@gmail.com>
2019-03-20 17:35:52 -0700

    Implement leetcode 108

--- bst_project/lib/leet_code_108.js
+++ bst_project/lib/leet_code_108.js
@@ -3,5 +3,19 @@
 
 
 function sortedArrayToBST(nums) {
+  if (!nums.length) return null;
 
+  let mid = parseInt(nums.length / 2);
+  let root = new TreeNode(nums[mid]);
+
+  let left = nums.slice(0, mid);
+  let right = nums.slice(mid+1);
+
+  let leftSubTree = sortedArrayToBST(left);
+  let rightSubTree = sortedArrayToBST(right);
+
+  root.left = leftSubTree;
+  root.right = rightSubTree;
+
+  return root;
 }
